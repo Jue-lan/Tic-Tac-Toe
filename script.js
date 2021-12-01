1294e40da9b384ca0cca71d0adc682030c4a72e8
jue-lan <juelan.brown@nyu.edu>
2021-11-30 21:20:37 -0500

    was able to collect an array of buttons selected for a color

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // warning only like 30% of this works...just trying to get ideas out of head
 let player1 = true
-
+let brownArray=[]
+let orangeArray=[]
 /* need array of win combos */
 
 const winCombos = [
@@ -18,7 +19,9 @@ const restartButton = document.querySelector(".restart")
 
 function restart() {
     //document.getElementById(".score-count").innerText = "0";
-    document.querySelectorAll(".child-board").forEach((button) => button.style.backgroundColor = "initial") // not sure if this will work. intend to change to default color
+    document.querySelectorAll(".child-board").forEach((button) => button.style.backgroundColor = "initial");
+    brownArray=[];
+    orangeArray=[]
 }
 
 restartButton.addEventListener("click", restart)
@@ -27,15 +30,19 @@ restartButton.addEventListener("click", restart)
 // psuedo code: if serious of button that match in color match any of the wining combos, alert  that, that color wins, clear board, and add count
 //or if all buttons ar enot default color, alert colors tie. and clear board
 // if neither nothing
-
+/*
 function clearBoard() {
     document.querySelectorAll(".child-board").style.backgroundColor = "initial" // not sure if this will work. intend to change to default color
-    colorBrown()
     
 }
+*/
 
+/*
 function gameEnd(){
+    for (let i = 0; i < winCombos.length; i++){
+        if 
 }
+*/
 
 /* need function that changes button color based on player being played */
 const player1Button = document.querySelector(".swtich-Player1")
@@ -68,6 +75,8 @@ boardButton.forEach((button) => button.addEventListener("click", selection))
 
 function colorBrown(){
     event.target.style.backgroundColor= "brown"
+    let number= event.target.id
+    brownArray.push(number)
 /*  
         document.querySelector(".swtich-Player1").style.backgroundColor = "brown"
         document.querySelector(".swtich-Player2").style.backgroundColor = "initial"
@@ -76,6 +85,8 @@ function colorBrown(){
 
 function colorOrange(){
     event.target.style.backgroundColor= "orange"
+    let number= event.target.id
+    orangeArray.push(number)
 /*    
     document.querySelector(".swtich-Player1").style.backgroundColor = "initial"
     document.querySelector(".swtich-Player2").style.backgroundColor = "orange"
