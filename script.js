b0ecf9aa561c62b47f27d1bc402473f053aa2faf
jue-lan <juelan.brown@nyu.edu>
2021-12-01 11:35:28 -0500

    scoping issue discovered and color swap works

--- script.js
+++ script.js
@@ -29,6 +29,9 @@ function restart() {
 
 restartButton.addEventListener("click", restart)
 
+
+/* player tallied picks and end game function*/
+
 let tallyBrown= 0
 let tallyOrange= 0
 
@@ -41,25 +44,27 @@ function gameEnd(){
         for (let k = 0; k < 3 ; k++){
             if (brownArray.includes(winCombos[i][k])){
                 tallyBrown++
+                console.log(tallyBrown)
             } //else {tallyBrown= 0}
             if (orangeArray.includes(winCombos[i][k])){
                 tallyOrange++
+                console.log(tallyOrange)
             } //else {tallyOrange= 0}
         }
-
+        console.log(tallyBrown)
+        console.log(tallyOrange)
         if (tallyBrown === 3){ 
             console.log("Player 1 wins!! New game.")
-            restart()
+            //restart()
         } else if (tallyOrange === 3){
             console.log("Player 2 wins!! New game.")
-            restart()
+            //restart()
         } 
     }
 }
 
 
-/* need function that changes button color based on player being played */
-
+/* board buttons and player selections and color fuctions*/
 
 const boardButton = document.querySelectorAll(".child-board")
 
@@ -67,10 +72,26 @@ function selection() {
 console.log("before if")
         if (player1){// *Here* I can't get color to swap back. It swaps once and sticks to orange. using console.log 
             //I can read that the true false staments alternate, but the color selected doesn't match the command
+            
+            function colorBrown(){
+                // if (event.target.style.backgroundColor=="initial" ){
+                     event.target.style.backgroundColor= "brown"
+                     let number= event.target.id
+                     brownArray.push(number)
+                // }
+             }
             boardButton.forEach((button) => button.addEventListener("click", colorBrown));
             player1 = false;
             console.log(" if true")
         } else {
+
+            function colorOrange(){
+                //   if (event.target.style.backgroundColor=="initial"){
+                       event.target.style.backgroundColor= "orange"
+                       let number= event.target.id
+                       orangeArray.push(number)
+                       //}
+               }
             boardButton.forEach((button) => button.addEventListener("click", colorOrange));
             player1 = true;
             console.log(" if false")
@@ -82,23 +103,3 @@ console.log("before if")
 
 boardButton.forEach((button) => button.addEventListener("click", selection))
 
-/*need function that switches to player when selected ... and possibly displays it*/
-
-function colorBrown(){
-   // if (event.target.style.backgroundColor=="initial" ){
-        event.target.style.backgroundColor= "brown"
-        let number= event.target.id
-        brownArray.push(number)
-        // player1 = false;
-   // }
-
-
-}
-
-function colorOrange(){
- //   if (event.target.style.backgroundColor=="initial"){
-        event.target.style.backgroundColor= "orange"
-        let number= event.target.id
-        orangeArray.push(number)
-        //player1 = true;}
-}
