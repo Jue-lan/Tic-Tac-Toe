5f967061de3ccc6e2912defb40e767e8ae7d56f5
jue-lan <juelan.brown@nyu.edu>
2021-12-01 14:26:00 -0500

    cleaned what I could from presentation

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let player1 = true
 let brownArray=[]
 let orangeArray=[]
 
-/* need array of win combos */
+/*array of win combos */
 
 const winCombos = [
     [0,1,2],
@@ -15,7 +15,7 @@ const winCombos = [
     [0,4,8],
     [2,4,6]
 ]
-/* need button that restarts game */
+/*restarts game */
 const restartButton = document.querySelector(".restart")
 
 function restart() {
@@ -27,8 +27,8 @@ function restart() {
 }
 
 restartButton.addEventListener("click", restart)
-/* player tallied picks and end game function*/
 
+/* player tallied picks and end game function*/
 let tallyBrown= 0
 let tallyOrange= 0
 
@@ -37,19 +37,13 @@ function gameEnd(){
     event.preventDefault()
     for (let i = 0; i < winCombos.length; i++){
         for (let k = 0; k < 3 ; k++){
-            if (brownArray.includes(winCombos[i][k])){ // this is not working, it isn't tallying
+            if (brownArray.includes(winCombos[i][k] )){ // this is not working, it isn't tallying
                 tallyBrown++
-                console.log(tallyBrown)// this is not being logged
             } else {tallyBrown= 0}
-            if (orangeArray.includes(winCombos[i][k])){
+            if (orangeArray.includes(winCombos[i][k])){ // maybe i need another way of loop around both here
                 tallyOrange++
-                console.log(tallyOrange)
             } else {tallyOrange= 0}
         }
-        // console.log(tallyBrown) // for debugging only
-        // console.log(brownArray) //debug
-        // console.log(orangeArray) 
-        // console.log(tallyOrange)
         if (tallyBrown >= 3){ 
             console.log("Player 1 wins!! New game.")
             //restart()
@@ -64,31 +58,26 @@ function gameEnd(){
 const boardButton = document.querySelectorAll(".child-board")
 
 function selection() {
-console.log("before if") // for debugging only
         if (player1){      
             function colorBrown(){
                      event.target.style.backgroundColor= "brown"
-                     let numberb= event.target.id
-                     brownArray.push(numberb) //why is it push all the numbers? another scoping issue?
+                     let numberB= event.target.id
+                     brownArray.push(numberB) //why is it push all the numbers? another scoping issue?
             }
             boardButton.forEach((button) => button.addEventListener("click", colorBrown));
             player1 = false;
-            console.log(" if true")// for debugging only
         } else {
             function colorOrange(){
                        event.target.style.backgroundColor= "orange"
                        console.log(event.target.id ) //with this touch point,  I can see that no matter if its if or else the array pushes the ID number
-                       let numbero= event.target.id
-                       
-                       orangeArray.push(numbero)
+                       let numberO= event.target.id
+                       orangeArray.push(numberO)
             }
             boardButton.forEach((button) => button.addEventListener("click", colorOrange));
             player1 = true;
-            console.log(" if false")// for debugging only
         } 
    event.preventDefault()
    gameEnd()
-   console.log(" after game function")// for debugging only
 }
 
 boardButton.forEach((button) => button.addEventListener("click", selection))
